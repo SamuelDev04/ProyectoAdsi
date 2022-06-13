038cc96daaa05a02db09d4cc47944b3651f06f7e
Sebastian <sebsalas@misena.edu.co>
2022-06-13 16:42:16 -0500

    Presentacion modificada

--- view/module/presentation.php
+++ view/module/presentation.php
@@ -1,7 +1,7 @@
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
 
-    <!--<img src="view/img/image.jpg" width="100%" height="100%">-->
+    <img src="view/img/Logo2.png" width="100%" height="100%">
         
 
   </div>
