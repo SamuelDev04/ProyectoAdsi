dbdbda73ffb48690259e94ee4f23988051696ec4
Samuel Y <syepes779@misena.edu.co>
2022-07-15 12:47:17 -0500

    Db para mis compas

--- db_rikopollo.sql
+++ db_rikopollo.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 15-07-2022 a las 08:15:13
+-- Tiempo de generación: 15-07-2022 a las 19:46:18
 -- Versión del servidor: 10.4.21-MariaDB
 -- Versión de PHP: 8.0.11
 
